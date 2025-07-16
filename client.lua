6f051b393afc0bf078ef1fa8f879f6c0cb290ce2
LeonS2k909 <leon2k909@outlook.com>
2025-07-16 08:09:08 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,19 @@
--- Converted from ox_target to qb-target for qb-policeaimissions
+-- client.lua for qb-policeaimissions
 
 local QBCore = exports['qb-core']:GetCoreObject()
 
+-- Make sure Config is loaded
+if not Config then
+    Config = {}
+    Config.PedLocation = vector3(-1105.75, -826.99, 14.29)
+    Config.PedHeading = 124.5
+end
+
 local targetPed = nil
 
-CreateThread(function()
+local function SpawnAIPed()
+    if targetPed and DoesEntityExist(targetPed) then return end
+
     RequestModel(`s_m_m_fiboffice_01`)
     while not HasModelLoaded(`s_m_m_fiboffice_01`) do Wait(0) end
 
@@ -27,6 +36,14 @@ CreateThread(function()
         },
         distance = 2.5
     })
+end
+
+RegisterNetEvent("policeai:client:SpawnAIPed", function()
+    SpawnAIPed()
+end)
+
+CreateThread(function()
+    SpawnAIPed()
 end)
 
 RegisterNetEvent("policeai:client:OpenMenu", function()
@@ -55,6 +72,24 @@ RegisterNetEvent("policeai:client:OpenMenu", function()
 end)
 
 RegisterNetEvent("policeai:client:StartMission", function()
-    -- Mission logic goes here
     QBCore.Functions.Notify("Mission started!", "success")
+    TriggerEvent("policeai:client:SendFightDispatch")
+    -- Insert AI fight logic here
+end)
+
+RegisterNetEvent("policeai:client:SendFightDispatch", function()
+    exports['ps-dispatch']:CustomAlert({
+        coords = GetEntityCoords(PlayerPedId()),
+        message = "10-10 | Fight in Progress",
+        dispatchCode = "10-10",
+        description = "Suspects engaged in a fight",
+        radius = 45,
+        sprite = 650,
+        color = 1,
+        scale = 1.2,
+        length = 6000,
+        sound = true,
+        caller = "Civilian",
+        job = {"police", "leo"},
+    })
 end)
